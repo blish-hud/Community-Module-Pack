818e34873a4a3ff5b1d50ed2bd7df760d0a03f5f
Andreas G <andreas.gaertner93@gmail.com>
2019-07-14 00:48:09 +0200

    deleted Kill Proof Module from master

--- Kill Proof Module/BuildScripts/CopyRef.ps1
+++
@@ -1,9 +0,0 @@
-﻿param ([string]$project, [string]$output)
-
-Write-Output "CopyRef will be using the following directories:"
-Write-Output "$($project)ref\*"
-Write-Output "$($project)$($output)ref"
-
-robocopy "$($project)ref " "$($project)$($output)ref " /MIR
-
-Write-Output "Directory 'ref' has been copied over!"
