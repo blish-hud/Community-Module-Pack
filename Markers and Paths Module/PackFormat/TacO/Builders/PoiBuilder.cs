7ca0d04086db6dcfd466ce18e95b266c801b2c3c
Bennieboj <4215739+bennieboj@users.noreply.github.com>
2020-05-02 19:15:12 +0200

    add routes, initial attempt

--- Markers and Paths Module/PackFormat/TacO/Builders/PoiBuilder.cs
+++ Markers and Paths Module/PackFormat/TacO/Builders/PoiBuilder.cs
@@ -6,27 +6,35 @@ using System.Threading.Tasks;
 using System.Xml;
 using Blish_HUD;
 using Blish_HUD.Pathing.Content;
+using Blish_HUD.Pathing.Trails;
 using NanoXml;
 
-namespace Markers_and_Paths_Module.PackFormat.TacO.Builders {
-    public static class PoiBuilder {
+namespace Markers_and_Paths_Module.PackFormat.TacO.Builders
+{
+    public static class PoiBuilder
+    {
 
         private static readonly Logger Logger = Logger.GetLogger(typeof(PoiBuilder));
 
-        private const string ELEMENT_POITYPE_POI   = "poi";
+        private const string ELEMENT_POITYPE_POI = "poi";
         private const string ELEMENT_POITYPE_TRAIL = "trail";
         private const string ELEMENT_POITYPE_ROUTE = "route";
 
-        public static void UnpackPathable(NanoXmlNode pathableNode, PathableResourceManager pathableResourceManager, PathingCategory rootCategory) {
-            switch (pathableNode.Name.ToLowerInvariant()) {
+        public static void UnpackPathable(NanoXmlNode pathableNode, PathableResourceManager pathableResourceManager, PathingCategory rootCategory)
+        {
+            switch (pathableNode.Name.ToLowerInvariant())
+            {
                 case ELEMENT_POITYPE_POI:
                     var poiAttributes = AttributeBuilder.FromNanoXmlNode(pathableNode);
 
                     var newPoiMarker = new Pathables.TacOMarkerPathable(poiAttributes, pathableResourceManager, rootCategory);
 
-                    if (newPoiMarker.SuccessfullyLoaded) {
+                    if (newPoiMarker.SuccessfullyLoaded)
+                    {
                         MarkersAndPathsModule.ModuleInstance._currentReader.RegisterPathable(newPoiMarker);
-                    } else {
+                    }
+                    else
+                    {
                         Logger.Warn("Failed to load marker: {markerInfo}", poiAttributes);
                     }
                     break;
@@ -36,15 +44,35 @@ namespace Markers_and_Paths_Module.PackFormat.TacO.Builders {
 
                     var newPathTrail = new Pathables.TacOTrailPathable(trailAttributes, pathableResourceManager, rootCategory);
 
-                    if (newPathTrail.SuccessfullyLoaded) {
+                    if (newPathTrail.SuccessfullyLoaded)
+                    {
                         MarkersAndPathsModule.ModuleInstance._currentReader.RegisterPathable(newPathTrail);
-                    } else {
+                    }
+                    else
+                    {
                         Logger.Warn("Failed to load trail: {trailInfo}", trailAttributes);
                     }
                     break;
 
                 case ELEMENT_POITYPE_ROUTE:
-                    Logger.Warn("Support for routes has not been added yet. They have been skipped.");
+                    //var routeAttributes = AttributeBuilder.FromNanoXmlNode(pathableNode);
+
+                    //var newRoute = new Pathables.TacOTrailPathable(routeAttributes, pathableResourceManager, rootCategory);
+
+                    //if (newRoute.SuccessfullyLoaded)
+                    //{
+                    //    MarkersAndPathsModule.ModuleInstance._currentReader.RegisterPathable(newRoute);
+                    //}
+                    //else
+                    //{
+                    //    Logger.Warn("Failed to load trail: {trailInfo}", routeAttributes);
+                    //}
+
+                    var poisInRoute = pathableNode.SelectNodes("poi");
+                    for (int i = 0; i < poisInRoute.Count(); i++)
+                    {
+                        UnpackPathable(poisInRoute[i], pathableResourceManager, rootCategory);
+                    }
                     break;
 
                 default:
---
+++ Markers and Paths Module/PackFormat/TacO/Pathables/TacORoutePathable.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using Blish_HUD;
+using Blish_HUD.Pathing;
+using Blish_HUD.Pathing.Content;
+using Blish_HUD.Pathing.Entities;
+using Blish_HUD.Pathing.Format;
+using Markers_and_Paths_Module.PackFormat.TacO.Behavior;
+using Microsoft.Xna.Framework;
+
+namespace Markers_and_Paths_Module.PackFormat.TacO.Pathables
+{
+    public sealed class TacORoutePathable : LoadedMarkerPathable, ITacOPathable
+    {
+
+        private const float DEFAULT_HEIGHTOFFSET = 1.5f;
+        private const float DEFAULT_ICONSIZE = 2f;
+
+        private string _type;
+        private PathingCategory _category;
+        private string _name;
+        private bool _backwardDirection;
+
+        private TacOBehavior _tacOBehavior;
+
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                if (SetProperty(ref _type, value))
+                {
+                    _category = _rootCategory.GetOrAddCategoryFromNamespace(_type);
+                    OnPropertyChanged(nameof(Category));
+                }
+            }
+        }
+
+        public PathingCategory Category
+        {
+            get => _category;
+            set
+            {
+                if (SetProperty(ref _category, value))
+                {
+                    _type = _category.Namespace;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+        public bool BackwardDirection
+        {
+            get => _backwardDirection;
+            set => SetProperty(ref _backwardDirection, value);
+        }
+
+        private readonly PathableAttributeCollection _sourceAttributes;
+        private readonly PathingCategory _rootCategory;
+
+        public TacORoutePathable(PathableAttributeCollection sourceAttributes, PathableResourceManager packContext, PathingCategory rootCategory) : base(packContext)
+        {
+            _sourceAttributes = sourceAttributes;
+            _rootCategory = rootCategory;
+
+            this.PropertyChanged += TacOMarkerPathable_PropertyChanged;
+
+            BeginLoad();
+        }
+
+        private void TacOMarkerPathable_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            _tacOBehavior?.PushPathableStateChange(e.PropertyName);
+        }
+
+        protected override void BeginLoad()
+        {
+            LoadAttributes(_sourceAttributes);
+        }
+
+        protected override void PrepareAttributes()
+        {
+            // Type
+            RegisterAttribute("type", attribute => (!string.IsNullOrEmpty(this.Type = attribute.Value.Trim())));
+
+            // Name
+            RegisterAttribute("Name", attribute => (!string.IsNullOrEmpty(this.Name = attribute.Value.Trim())));
+
+            // BackwardDiraction
+            RegisterAttribute("BackwardDirection", delegate (PathableAttribute attribute) {
+                if (!InvariantUtil.TryParseInt(attribute.Value, out int fOut)) return false;
+
+                this.BackwardDirection = fOut == 1;
+                return true;
+            });
+
+            base.PrepareAttributes();
+        }
+
+        protected override bool FinalizeAttributes(Dictionary<string, LoadedPathableAttributeDescription> attributeLoaders)
+        {
+            // Process attributes from type category first
+            if (_category != null)
+            {
+                ProcessAttributes(_category.Attributes);
+            }
+
+            _category?.AddPathable(this);
+
+            // Let base finalize attributes
+            return base.FinalizeAttributes(attributeLoaders);
+        }
+
+    }
+}
