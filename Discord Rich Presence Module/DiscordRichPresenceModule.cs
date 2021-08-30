16df3c605d8f85ec04c7826b72a18308f391003b
agaertner <agaertner@users.noreply.github.com>
2021-08-30 14:41:44 +0200

    show display name of single sector maps

--- Discord Rich Presence Module/DiscordRichPresenceModule.cs
+++ Discord Rich Presence Module/DiscordRichPresenceModule.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Threading.Tasks;
-using Blish_HUD;
+﻿using Blish_HUD;
 using Blish_HUD.Modules;
 using Blish_HUD.Modules.Managers;
 using Blish_HUD.Settings;
 using DiscordRPC;
 using Gw2Sharp.WebApi.V2.Models;
 using Microsoft.Xna.Framework;
-
-namespace Discord_Rich_Presence_Module {
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord_Rich_Presence_Module.Utils;
+namespace Discord_Rich_Presence_Module
+{
 
     [Export(typeof(Module))]
     public class DiscordRichPresenceModule : Module {
@@ -85,27 +87,52 @@ namespace Discord_Rich_Presence_Module {
             // Stop-gap until we switch this over to the other Discord app where
             // map names are added to Discord via map ID instead of by name
             Gw2ApiManager.Gw2ApiClient.V2.Maps.GetAsync(e.Value)
-                         .ContinueWith(mapTask => {
-                                           if (!mapTask.IsFaulted && mapTask.Result != null) {
-                                               UpdateDetails(mapTask.Result);
-                                           }
-                                       });
+                         .ContinueWith(mapTask =>
+                         {
+                             if (mapTask.IsFaulted || !mapTask.IsCompleted)
+                                 return;
+                             var map = mapTask.Result;
+                             UpdateDetails(map);
+                         });
         }
 
-        private void UpdateDetails(Map map) {
+        private async Task<IEnumerable<ContinentFloorRegionMapSector>> RequestSectors(int continentId, int floor, int regionId, int mapId)
+        {
+            try
+            {
+                return await Gw2ApiManager.Gw2ApiClient.V2.Continents[continentId].Floors[floor].Regions[regionId].Maps[mapId].Sectors.AllAsync();
+            }
+            catch (Gw2Sharp.WebApi.Exceptions.BadRequestException e)
+            {
+                Logger.Debug("{0} | The map id {1} does not exist on floor {2}.", e.GetType().FullName, mapId, floor);
+                return Enumerable.Empty<ContinentFloorRegionMapSector>();
+            }
+        }
+
+        private async void UpdateDetails(Map map) {
             if (map.Id <= 0) return;
+
+            var location = map.Name;
+
+            //Some instanced maps consist of just a single sector and hide their display name in it.
+            if (map.Name.Equals(map.RegionName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var defaultSector = (await RequestSectors(map.ContinentId, map.DefaultFloor, map.RegionId, map.Id)).FirstOrDefault();
+                if (defaultSector != null && !string.IsNullOrEmpty(defaultSector.Name))
+                    location = defaultSector.Name.Replace("<br>", " ");
+            }
 
             // rpcClient *shouldn't* be null at this point unless a rare race condition occurs
             // In the event that this occurs, it'll be resolved by the next loop
             _rpcClient?.SetPresence(new RichPresence() {
                 // Truncate length (requirements: https://discordapp.com/developers/docs/rich-presence/how-to)
                 Details = DiscordUtil.TruncateLength(GameService.Gw2Mumble.PlayerCharacter.Name, 128),
-                State   = DiscordUtil.TruncateLength($"in {map.Name}", 128),
+                State   = DiscordUtil.TruncateLength($"in {location}", 128),
                 Assets = new Assets() {
                     LargeImageKey = DiscordUtil.TruncateLength(_mapOverrides.ContainsKey(map.Id.ToString())
                                                                    ? _mapOverrides[map.Id.ToString()]
-                                                                   : DiscordUtil.GetDiscordSafeString(map.Name), 32),
-                    LargeImageText = DiscordUtil.TruncateLength(map.Name,                                                128),
+                                                                   : DiscordUtil.GetDiscordSafeString(location), 32),
+                    LargeImageText = DiscordUtil.TruncateLength(location,                                                128),
                     SmallImageKey  = DiscordUtil.TruncateLength(((MapType)map.Type.Value).ToString().ToLowerInvariant(), 32),
                     SmallImageText = DiscordUtil.TruncateLength(((MapType)map.Type.Value).ToString().Replace("_", " "),  128)
                 },
--- Discord Rich Presence Module/Utils/DiscordUtil.cs
+++ Discord Rich Presence Module/Utils/DiscordUtil.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-namespace Discord_Rich_Presence_Module {
+﻿using System.Text.RegularExpressions;
+namespace Discord_Rich_Presence_Module.Utils {
     public static class DiscordUtil {
 
         public static string TruncateLength(string value, int maxLength) {
