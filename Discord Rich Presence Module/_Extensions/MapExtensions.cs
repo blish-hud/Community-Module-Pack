a398f6fe83fcd85f09cb0b37ed422e81124255e6
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-09-10 12:47:24 +0200

    map table

---
+++ Discord Rich Presence Module/_Extensions/MapExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Gw2Sharp.WebApi.V2.Models;
+
+namespace Discord_Rich_Presence_Module
+{
+    public static class MapExtensions
+    {
+        /// <summary>
+        /// Gets a hash of the map's continent rectangle which can be used to identify copies of the same map.
+        /// </summary>
+        /// <param name="map">The map to get the hash of.</param>
+        /// <returns>
+        /// The first 8 characters of the SHA1 hash of the following string<br/>
+        /// <c>SHA1(&lt;continent_id&gt;&lt;continent_rect[0][0]&gt;&lt;continent_rect[0][1]&gt;&lt;continent_rect[1][0]&gt;&lt;continent_rect[1][1]&gt;)</c>
+        /// </returns>
+        public static string GetHash(this Map map)
+        {
+            var rpcHash = $"{map.ContinentId}{map.ContinentRect.TopLeft.X}{map.ContinentRect.TopLeft.Y}{map.ContinentRect.BottomRight.X}{map.ContinentRect.BottomRight.Y}";
+            return rpcHash.ToSHA1Hash().Substring(0, 8);
+        }
+    }
+}
---
+++ Discord Rich Presence Module/_Utils/TaskUtil.cs
@@ -0,0 +1,49 @@
+﻿using Flurl.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discord_Rich_Presence_Module
+{
+    public class TaskUtil
+    {
+        public static bool TryParseJson<T>(string json, out T result)
+        {
+            bool success = true;
+            var settings = new JsonSerializerSettings
+            {
+                Error = (_, args) => { success = false; args.ErrorContext.Handled = true; },
+                MissingMemberHandling = MissingMemberHandling.Error
+            };
+            result = JsonConvert.DeserializeObject<T>(json, settings);
+            return success;
+        }
+
+        public static async Task<(bool, T)> GetJsonResponse<T>(string request, int timeOutSeconds = 10)
+        {
+            try
+            {
+                var rawJson = await request.AllowHttpStatus(HttpStatusCode.NotFound).AllowHttpStatus("200").WithTimeout(timeOutSeconds).GetStringAsync();
+                return (TryParseJson<T>(rawJson, out var result), result);
+            }
+            catch (FlurlHttpTimeoutException ex)
+            {
+                DiscordRichPresenceModule.Logger.Warn(ex, $"Request '{request}' timed out.");
+            }
+            catch (FlurlHttpException ex)
+            {
+                DiscordRichPresenceModule.Logger.Warn(ex, $"Request '{request}' was not successful.");
+            }
+            catch (JsonReaderException ex)
+            {
+                DiscordRichPresenceModule.Logger.Warn(ex, $"Failed to deserialize requested content from \"{request}\"\n{ex.StackTrace}");
+            }
+            catch (Exception ex)
+            {
+                DiscordRichPresenceModule.Logger.Error(ex, $"Unexpected error while requesting '{request}'.");
+            }
+            return (false, default);
+        }
+    }
+}
