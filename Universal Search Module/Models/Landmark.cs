a8c96321393f945b315b1c00dfe9db167906922f
Xander Lamkins <dade@lamkins.org>
2023-01-29 12:31:40 -0500

    Cleared branch contents.

--- Universal Search Module/Models/Landmark.cs
+++
@@ -1,9 +0,0 @@
-﻿using Gw2Sharp.WebApi.V2.Models;
-
-namespace Universal_Search_Module.Models {
-    public class Landmark {
-        public ContinentFloorRegionMapPoi PointOfInterest { get; set; }
-
-        public ContinentFloorRegionMap Map { get; set; }
-    }
-}
